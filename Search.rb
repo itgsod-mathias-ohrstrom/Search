64dbd0d493b215b64509b2959628faf0c00b8b02
itgsod-mathias-ohrstrom <mathias.ohrstrom@Administrators-MacBook-Pro-5.local>
2015-03-04 11:48:23 +0100

    sweg

--- Search.rb
+++ Search.rb
@@ -1,3 +1,26 @@
-def swag
-	
+def search(list:, hc:)
+
+  if list.first > hc
+    p "Finns ej."
+    return "Finns ej"
+  elsif list.last < hc
+    p "Finns ej"
+    return "Finns ej"
+  end
+  until list[list.length/2] == hc
+      middle_index = list.length/2
+      middle_value = list[middle_index]
+      if middle_value > hc
+        list.delete_if { |value| value >= middle_value }
+        p "delete alt som är större"
+
+      else
+        list.delete_if { |value| value <= middle_value }
+        p "delete alt som är mindre"
+      end
+    p list
+  end
+  p hc
 end
+
+search(list: [2, 2, 4, 4, 5, 6, 7, 9, 9, 10] ,hc: 8)
